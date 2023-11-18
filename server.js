f89b4088e44e7ccd31189ba79e9e21b8771eec30
MilesTeg97 <64781653+MilesTeg97@users.noreply.github.com>
2023-11-18 18:51:08 +0100

    test

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ const STR_INVALID_TRANSFER_MODE = 'Invalid transfer mode, must be text';
 
 console.log('Initialising Node...')
 
+
+let crypto;
+try {
+  crypto = require('node:crypto');
+} catch (err) {
+  console.error('crypto support is disabled!');
+} 
+
 /*
 var http = require('http');
 http.createServer(function (req, res) {

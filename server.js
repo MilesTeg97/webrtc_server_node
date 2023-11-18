351d79eb9a5905daf5e8d2aabc369b7d015499c1
MilesTeg97 <64781653+MilesTeg97@users.noreply.github.com>
2023-11-18 17:45:18 +0100

    fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const STR_INVALID_TRANSFER_MODE = 'Invalid transfer mode, must be text';
 
 
 
-console. log('Initialising Node...')
+console.log('Initialising Node...')
 
 /*
 var http = require('http');
@@ -303,7 +303,7 @@ wss.on('connection', (ws) => {
 	});
 });
 
-console. log('Done! Signaling server is now listening on Port:' + PORT )
+console.log('Done! Signaling server is now listening on Port:' + PORT )
 
 
 const interval = setInterval(() => { // eslint-disable-line no-unused-vars
